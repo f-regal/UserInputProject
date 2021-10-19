01416d7adaed91d95d9eca98e1dfc750b216bdb8
f-regal <faysalregal@gmail.com>
2021-10-19 20:31:25 +0100

    firstCommit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import User from "./Components/User/User";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <section id="goal-form">
+        <User/>
+      </section>
+      
+    </div>
+  );
+}
+
+export default App;
